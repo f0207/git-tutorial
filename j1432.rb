49482ddd8b44312346cc88cfbac11fbfa151ed48
fukuhara <j1432@matsue-ct.jp>
2017-11-21 10:07:47 +0900

    edit j1432.rb part2 complete

--- j1432.rb
+++ j1432.rb
@@ -1,13 +1,13 @@
 #! /usr/bin/env ruby
 
 100.times{ |i|
-  if i%3==0 && i%5==0 then
-     puts FizzBuzz
-  elsif i%3==0 then
-     puts Fizz
-  elsif i%5==0 then
-     puts Buzz
+  if (i+1)%3==0 && (i+1)%5==0 then
+     puts "FizzBuzz"
+  elsif (i+1)%3==0 then
+     puts "Fizz"
+  elsif (i+1)%5==0 then
+     puts "Buzz"
   else
-     print i+"\n"
+     puts i+1
   end
 }
