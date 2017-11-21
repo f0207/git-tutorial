4bf8cc9287c148334a230e0d15d65b01f0de2b31
fukuhara <j1432@matsue-ct.jp>
2017-11-21 10:00:32 +0900

    edit j1432.rb part1

---
+++ j1432.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/env ruby
+
+100.times{ |i|
+  if i%3==0 && i%5==0 then
+     puts FizzBuzz
+  elsif i%3==0 then
+     puts Fizz
+  elsif i%5==0 then
+     puts Buzz
+  else
+     print i+"\n"
+  end
+}
