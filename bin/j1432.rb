bb6a529326860ceeb6cd66815bd339f8af0d046b
fukuhara <j1432@matsue-ct.jp>
2017-11-28 10:07:43 +0900

    make bin and move j1432.rb

---
+++ bin/j1432.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/env ruby
+
+100.times{ |i|
+  if (i+1)%3==0 && (i+1)%5==0 then
+     puts "FizzBuzz"
+  elsif (i+1)%3==0 then
+     puts "Fizz"
+  elsif (i+1)%5==0 then
+     puts "Buzz"
+  else
+     puts i+1
+  end
+}
