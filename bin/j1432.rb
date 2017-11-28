fe6a3a61564982a29a38a1e26021b4eb9406de46
fukuhara <j1432@matsue-ct.jp>
2017-11-28 10:18:39 +0900

    fix j1432.rb

--- bin/j1432.rb
+++ bin/j1432.rb
@@ -3,6 +3,8 @@
 100.times{ |i|
   if (i+1)%3==0 && (i+1)%5==0 then
      puts "FizzBuzz"
+  elsif (i+1)%7==0 then
+     puts "git"
   elsif (i+1)%3==0 then
      puts "Fizz"
   elsif (i+1)%5==0 then
